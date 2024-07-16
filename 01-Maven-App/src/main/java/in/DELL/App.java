73cc8a051bfe44df81cae47c997cb3f34032b713
Nagarajuchinni <ykeshava16@gmail.com>
2024-07-16 14:20:59 +0530

    commit to the pom

---
+++ 01-Maven-App/src/main/java/in/DELL/App.java
@@ -0,0 +1,13 @@
+package in.DELL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
